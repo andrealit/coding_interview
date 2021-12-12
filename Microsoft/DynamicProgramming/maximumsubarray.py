0961b01564eb1c6bd8c8051b3abfe67ccea47a61
Andrea Tongsak <atongsak@gmail.com>
2021-12-12 11:32:10 -0800

    Max Subarray

---
+++ Microsoft/DynamicProgramming/maximumsubarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        # define an array dp same as the size of nums, fill it with 0
+        dp = [0 for i in range(len(nums))]
+        # set the first to the first in nums
+        dp[0] = nums[0]
+        # find the maximum of the previous plus current OR the current
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i-1]+nums[i], nums[i])
+            
+        return max(dp)
